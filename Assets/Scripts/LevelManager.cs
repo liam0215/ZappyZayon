1e6b104e5027e5cdb7130cb6b6a20f05fa753aaa
Liam Arzola <liam0215@gmail.com>
2017-08-30 13:25:34 -0400

    Project Setup

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public Transform mainMenu, optionsMenu;
+    public static LevelManager levelManager;
+
+	public void LoadByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+
+	public void OptionsMenu(bool clicked)
+	{
+		optionsMenu.gameObject.SetActive(clicked);
+		mainMenu.gameObject.SetActive(!clicked);
+	}
+}
---
+++ Assets/Scripts/WordTyper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordTyper : MonoBehaviour {
+
+	public float delay = 0.1f;
+	public string fullText;
+	private string currentText = "";
+
+	// Use this for initialization
+	void Start (){
+		StartCoroutine("ShowText");
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("return"))
+        {
+            currentText = fullText;
+            this.GetComponent<Text>().text = currentText;
+            StopCoroutine("ShowText");
+        }
+    }
+
+    IEnumerator ShowText() {
+		for (int i = 0; i < fullText.Length; i++) {
+			currentText = fullText.Substring (0, i);
+			this.GetComponent<Text>().text = currentText;
+			yield return new WaitForSeconds(delay);
+		}
+	}
+}
