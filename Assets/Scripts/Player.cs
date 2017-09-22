7862ed4b0316ebc3afd9313fea1fed31fa2f5ec3
Liam Arzola <liam0215@gmail.com>
2017-09-22 16:46:01 -0400

    Finished player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed;
-
+    private Animator anim;
+    private int animDir = 1;
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -19,17 +20,42 @@ public class Player : MonoBehaviour {
     void Move()
     {
         float dx = 0.0f, dy = 0.0f;
-        if (Input.GetKey(KeyCode.W)) dy = speed;
-        if (Input.GetKey(KeyCode.S)) dy = -speed;
-        if (Input.GetKey(KeyCode.D)) dx = speed;
-        if (Input.GetKey(KeyCode.A)) dx = -speed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            dy = speed;
+            animDir = 0;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dy = -speed;
+            animDir = 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dx = speed;
+            animDir = 2;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dx = -speed;
+            animDir = 3;
+        }
 
-        if(dx != 0.0f && dy != 0.0f)
+        if (dx == 0.0f && dy == 0.0f)
+        {
+            anim.speed = 0;
+        } else
+        {
+            anim.speed = 1;
+        }
+
+        if (dx != 0.0f && dy != 0.0f)
         {
             dx *= Mathf.Cos(Mathf.PI / 4);
             dy *= Mathf.Sin(Mathf.PI / 4);
-        }
-        Debug.Log(dx + " " + dy + " " + speed);
+        } 
+
+        anim.SetInteger("dir", animDir);
         transform.Translate(dx * Time.deltaTime, dy * Time.deltaTime, 0);
     }
 }
